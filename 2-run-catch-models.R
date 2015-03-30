0a65a8312352134efefbeb339d8e7745a66911c2
Sean Anderson <sean@seananderson.ca>
2015-03-30 14:20:50 -0700

    Clean up CMSY RAM running code

--- 2-run-catch-models.R
+++ 2-run-catch-models.R
@@ -5,21 +5,23 @@ load_all("../datalimited")
 ramts <- readRDS("generated-data/ramts.rds")
 ramts <- ramts %>% filter(!is.na(c_touse)) # TODO won't this create gaps in years?
 
+message("Running CMSY fits to RAM database...")
 ram_cmsy <- ramts %>%
   filter(stockid %in% unique(ramts$stockid)[1:6]) %>%
   group_by(stockid) %>%
   do({
     message(.$stocklong[1])
     cmsy_out <- cmsy(
-      yr = .$year,
-      ct = .$c_touse,
+      yr             = .$year,
+      ct             = .$c_touse,
       prior_log_mean = .$log_mean[1],
-      prior_log_sd = .$log_sd[1],
-      start_r = resilience(.$res[1]),
-      sig_r = 0.05,
-      reps = 5e4)
-    bbmsy <- cmsy_out$biomass[, -1] / cmsy_out$schaefer$bmsy
+      prior_log_sd   = .$log_sd[1],
+      start_r        = resilience(.$res[1]),
+      sig_r          = 0.05,
+      reps           = 5e4)
+    bbmsy <- cmsy_out$biomass[, -1] / cmsy_out$quantities$bmsy
     bbmsy[is.infinite(bbmsy)] <- NA  # TODO investigate the -Inf values
-    bbmsy_out <- summarize_bbmsy(bbmsy, na.rm = TRUE, log = FALSE)
-    data.frame(year = .$year, c_touse = .$c_touse, bbmsy_out)
+    # TODO switch log to TRUE:
+    bbmsy_out <- summarize_bbmsy(bbmsy, na.rm = TRUE, log = FALSE) # TODO investigate the negative values
+    data.frame(year = .$yr, c_touse = .$c_touse, bbmsy_out)
   }) %>% as.data.frame
