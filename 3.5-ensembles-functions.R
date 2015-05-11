879d47b5276e5d25a918dad746d40b4d82aafadd
Sean Anderson <sean@seananderson.ca>
2015-05-11 15:18:47 -0700

    Revise simulation-based ensemble script

---
+++ 3.5-ensembles-functions.R
@@ -0,0 +1,43 @@
+# General purpose functions to be used throughout
+
+# A general function for cross-validation testing ensemble models:
+cross_val_ensembles <- function(.n, dat, fraction_train = 0.5,
+  formula = "log(bbmsy_true_mean) ~ CMSY + COM.SIR + Costello + SSCOM + LH",
+  geo_mean = TRUE,
+  individual_models = c("CMSY", "COM.SIR", "Costello", "SSCOM"),
+  id = "mean", cache_folder = "generated-data/cv-sim/") {
+
+  cv_ids_set <- FALSE # legacy code, leaving in in case needed for RAM stocks
+  while(!cv_ids_set) {
+    nstocks <- length(unique(dat$stock_id))
+    train_ids <- sample(nstocks, round(nstocks * fraction_train))
+    test_ids <- seq_len(nstocks)[-train_ids]
+
+    train_stock_ids <- unique(dat$stock_id)[train_ids]
+    test_stock_ids <- unique(dat$stock_id)[test_ids]
+
+    train_dat <- filter(dat, stock_id %in% train_stock_ids)
+    test_dat <- filter(dat, stock_id %in% test_stock_ids)
+
+    cv_ids_set <- TRUE
+  }
+
+  m_gbm <- gbm::gbm(as.formula(formula),
+    data = train_dat, n.trees = 3000L, interaction.depth = 3, shrinkage = 0.001,
+    distribution = "gaussian")
+
+  saveRDS(m_gbm, file = paste0(cache_folder, id, "-", .n, "-gbm.rds"))
+
+  test_dat$gbm_ensemble <- tryCatch({gbm::predict.gbm(m_gbm,
+    n.trees = m_gbm$n.trees, newdata = test_dat)},
+    error = function(e) rep(NA, nrow(test_dat)))
+
+    if (geo_mean) {
+      test_dat$mean_ensemble <- exp(rowMeans(log(test_dat[, individual_models])))
+    } else {
+      test_dat$mean_ensemble <- rowMeans(test_dat[, individual_models])
+    }
+
+  test_dat$test_iter <- .n
+  test_dat
+}
--- 4-simulation-ensembles.R
+++ 4-simulation-ensembles.R
@@ -1,31 +1,32 @@
-library(dplyr)
-library(ggplot2)
-if (!file.exists("generated-data/cv-gbms/")) system("mkdir generated-data/cv-gbms")
+library("dplyr")
+library("ggplot2")
+source("3.5-ensembles-functions.R")
+
+if (!file.exists("generated-data/cv-sim/")) system("mkdir generated-data/cv-sim")
 
 dsim <- readRDS("raw-data/batch1-results.rds")
 
+# extend the stock_id to be truly unique per scenario:
+# (note that there are still multiple iterations with the same stock_id)
 dsim <- dsim %>%
   transform(stock_id =
-  paste0(stock_id, "_sigmaC_", sigmaC, "_sigmaR_", sigmaR, "_lh", LH, "_it_", iter)) %>%
-  arrange(stock_id, iter, year)
+  paste0(stock_id, "_sigmaC_", sigmaC, "_sigmaR_", sigmaR, "_lh", LH)) %>%
+  arrange(stock_id, iter, year) # critical since not all in order
 
+# summarise the mean and slope in the last N years:
 if(!file.exists("generated-data/dsim_sum.rds")) {
   dsim_sum <- dsim %>%
-    group_by(stock_id, method_id, LH) %>%
+    group_by(stock_id, method_id, iter) %>%
     do({
-
       years_mean <- 3
       .n <- nrow(.)
       i <- seq(.n-(years_mean-1), .n)
-
       bbmsy_true_mean = mean(.$b_bmsy_true[i])
       bbmsy_est_mean = mean(.$b_bmsy_est[i])
-
       ytrue <- .$b_bmsy_true[i]
       yest <- .$b_bmsy_est[i]
       bbmsy_true_slope <- coef(mblm::mblm(ytrue ~ i))[[2]]
       bbmsy_est_slope <- coef(mblm::mblm(yest ~ i))[[2]]
-
       data.frame(bbmsy_true_mean, bbmsy_est_mean, bbmsy_true_slope, bbmsy_est_slope)
     }) %>% as.data.frame
     saveRDS(dsim_sum, file = "generated-data/dsim_sum.rds")
@@ -33,27 +34,29 @@ if(!file.exists("generated-data/dsim_sum.rds")) {
   dsim_sum <- readRDS("generated-data/dsim_sum.rds")
 }
 
+# join in some characteristics that we'll use in models:
 dsim_meta <- dsim %>%
-  group_by(stock_id) %>%
+  group_by(stock_id, iter) %>%
   summarise(
+    LH = LH[1],
     max_catch = max(catch),
     total_catch = sum(catch))
-
 dsim_sum <- inner_join(dsim_sum, dsim_meta)
 
-trues <- select(dsim_sum, stock_id, method_id, bbmsy_true_mean, bbmsy_true_slope)
-trues <- trues[!duplicated(trues), ]
+# save a data frame of 'true' operating model values to merge in:
+trues <- select(dsim_sum, stock_id, iter, bbmsy_true_mean, bbmsy_true_slope)
+trues <- trues[!duplicated(trues), ] # one value per operating model stock_id
 
-d_mean <- reshape2::dcast(dsim_sum, stock_id + LH ~ method_id,
+# switch from long to wide format for modelling:
+d_mean <- reshape2::dcast(dsim_sum, stock_id + iter + LH ~ method_id,
   value.var = "bbmsy_est_mean")  %>%
   inner_join(select(trues, -bbmsy_true_slope))
-
-d_slope <- reshape2::dcast(dsim_sum, stock_id + LH ~ method_id,
+d_slope <- reshape2::dcast(dsim_sum, stock_id + iter + LH ~ method_id,
   value.var = "bbmsy_est_slope") %>%
   inner_join(select(trues, -bbmsy_true_mean))
 
+# run a model on all the data to generate data for partial dependence plots:
 m <- gbm::gbm(log(bbmsy_true_mean) ~ CMSY + COM.SIR + Costello + SSCOM + LH,
-#m <- gbm::gbm(bbmsy_true_mean ~ CMSY + COM.SIR + Costello + SSCOM + LH,
   data = d_mean, n.trees = 3000L, interaction.depth = 3, shrinkage = 0.001)
 partial <- plyr::ldply(seq_len(5), function(i) {
   dd <- gbm::plot.gbm(m, i.var = i, return.grid = TRUE)
@@ -63,104 +66,93 @@ partial <- plyr::ldply(seq_len(5), function(i) {
   dd
 })
 
+# partial dependence plot:
 ggplot(partial, aes(predictor_value, y)) + geom_line() + facet_wrap(~predictor) + xlim(0, 3)
 
-# a general function for cross-validation testing ensemble models:
-
-cross_val_ensembles <- function(.n, dat, fraction_train = 0.5,
-  formula = "log(bbmsy_true_mean) ~ CMSY + COM.SIR + Costello + SSCOM + LH",
-  id = "mean", geomean = TRUE) {
-
-  cv_ids_set <- FALSE # legacy code, leaving in in case needed for RAM stocks
-  while(!cv_ids_set) {
-    nstocks <- length(unique(dat$stock_id))
-
-    train_ids <- sample(nstocks, round(nstocks * fraction_train))
-    test_ids <- seq_len(nstocks)[-train_ids]
-
-    train_stock_ids <- unique(dat$stock_id)[train_ids]
-    test_stock_ids <- unique(dat$stock_id)[test_ids]
-
-    train_dat <- filter(dat, stock_id %in% train_stock_ids)
-    test_dat <- filter(dat, stock_id %in% test_stock_ids)
-
-    # make sure we have all 'test' species categories in our 'train' data
-    # or the lm() prediction will fail
-    cv_ids_set <- TRUE
-  }
-
-  m_gbm <- gbm::gbm(as.formula(formula),
-    data = train_dat, n.trees = 2000L, interaction.depth = 3, shrinkage = 0.001,
-    distribution = "gaussian")
-
-  saveRDS(m_gbm, file = paste0("generated-data/cv-gbms/", id, "-", .n, "-gbm.rds"))
-
-  test_dat$gbm_ensemble <- tryCatch({gbm::predict.gbm(m_gbm,
-    n.trees = m_gbm$n.trees, newdata = test_dat)},
-    error = function(e) rep(NA, length(test_ids)))
-
-  if (geomean) {
-  test_dat$mean_ensemble <- exp(c(log(test_dat$CMSY), log(test_dat$COM.SIR),
-    log(test_dat$Costello), log(test_dat$SSCOM)))
-  } else {
-  test_dat$mean_ensemble <- c(test_dat$CMSY, test_dat$COM.SIR,
-    test_dat$Costello, test_dat$SSCOM)
-  }
-
-  test_dat$test_iter <- .n
-  test_dat
-}
-
+# work through cross validation of ensemble models:
 library("doParallel")
 registerDoParallel(cores = 4)
 
-cv_sim_mean <- plyr::ldply(seq_len(8), .parallel = TRUE,
+cv_sim_mean <- plyr::ldply(seq_len(16), .parallel = TRUE,
   .fun = function(.n)
-    cross_val_ensembles(.n = .n, dat = d_mean, id = "sim-mean",
+    cross_val_ensembles(.n = .n, dat = d_mean, geo_mean = TRUE, id = "sim-mean",
       formula = "log(bbmsy_true_mean) ~ CMSY + COM.SIR + Costello + SSCOM + LH"))
 cv_sim_mean$gbm_ensemble <- exp(cv_sim_mean$gbm_ensemble)
 
-cv_sim_slope <- plyr::ldply(seq_len(8), .parallel = TRUE,
+cv_sim_slope <- plyr::ldply(seq_len(4), .parallel = TRUE,
   .fun = function(.n)
-    cross_val_ensembles( .n = .n, dat = d_slope, id = "sim-slope",
-      geomean = FALSE,
-      formula = "bbmsy_true_slope ~ CMSY + COM.SIR + Costello + SSCOM + LH"))
+    cross_val_ensembles(.n = .n, dat = d_slope, geo_mean = FALSE, id = "sim-slope",
+     formula = "bbmsy_true_slope ~ CMSY + COM.SIR + Costello + SSCOM + LH"))
 
-############## fix from here:
-# now melt all the methods:
+# -------------------------------------------------------
+# now switch to long format data, summarize, and compare:
 cv_sim_mean_long <- reshape2::melt(cv_sim_mean,
-  id.vars = c("stock_id", "test_iter", "LH", "bbmsy_true_mean"),
-  variable.name = "method_id", value.name = "bbmsy_est_mean")
+  id.vars = c("stock_id", "iter", "test_iter", "LH", "bbmsy_true_mean"),
+  variable.name = "method_id", value.name = "bbmsy_est") %>%
+  rename(bbmsy_true = bbmsy_true_mean) %>%
+  mutate(
+    type = "mean",
+    bbmsy_true_trans = log(bbmsy_true),
+    bbmsy_est_trans = log(bbmsy_est))
 
 cv_sim_slope_long <- reshape2::melt(cv_sim_slope,
-  id.vars = c("stock_id", "test_iter", "LH", "bbmsy_true_slope"),
-  variable.name = "method_id", value.name = "bbmsy_est_slope")
+  id.vars = c("stock_id", "iter", "test_iter", "LH", "bbmsy_true_slope"),
+  variable.name = "method_id", value.name = "bbmsy_est") %>%
+  rename(bbmsy_true = bbmsy_true_slope) %>%
+  mutate(
+    type = "slope",
+    bbmsy_true_trans = bbmsy_true,
+    bbmsy_est_trans = bbmsy_est)
 
-ggplot(cv_sim_mean_long, aes(bbmsy_true_mean, bbmsy_est_mean)) +
-  geom_point(alpha = 0.03) +
+ggplot(cv_sim_mean_long, aes(bbmsy_true, bbmsy_est)) +
+  geom_point(alpha = 0.01) +
     facet_wrap(~method_id) + ylim(0, 3) + xlim(0, 3)
 
-ggplot(cv_sim_slope_long, aes(bbmsy_true_slope, bbmsy_est_slope)) +
-  geom_point(alpha = 0.03) +
+ggplot(cv_sim_slope_long, aes(bbmsy_true, bbmsy_est)) +
+  geom_point(alpha = 0.01) +
     facet_wrap(~method_id) + xlim(-.5, .5) + ylim(-.5, .5)
 
-cv_sim_mean_long %>% group_by(method_id) %>%
-  summarise(spearman = cor(bbmsy_true_mean, bbmsy_est_mean, method = "spearman",
-    use = "pairwise.complete.obs"))
+cv_sim_long <- suppressWarnings(
+  dplyr::bind_rows(cv_sim_mean_long, cv_sim_slope_long))
 
-cv_sim_slope_long %>% group_by(method_id) %>%
-  summarise(spearman = cor(bbmsy_true_slope, bbmsy_est_slope, method = "spearman",
-    use = "pairwise.complete.obs"))
+cors <- cv_sim_long %>% group_by(method_id, type) %>%
+  summarise(spearman = cor(bbmsy_true_trans, bbmsy_est_trans, method = "spearman",
+    use = "pairwise.complete.obs")) %>% as.data.frame %>%
+  mutate(spearman = round(spearman, 4)) %>%
+  arrange(type, -spearman)
 
-re <- cv_sim_mean_long %>% mutate(
-   sq_er = (log(bbmsy_est_mean) - log(bbmsy_true_mean))^2,
-  #sq_er = ((bbmsy_est_mean) - (bbmsy_true_mean))^2,
-   re = (log(bbmsy_est_mean) - log(bbmsy_true_mean)) / log(bbmsy_true_mean)) %>%
-  #re = ((bbmsy_est_mean) - (bbmsy_true_mean)) / (bbmsy_true_mean)) %>%
-  group_by(method_id, test_iter) %>%
+ggplot(cors, aes(x = method_id, xend = method_id, yend = spearman)) +
+  geom_segment(y = 0, lwd = 1.2) + facet_wrap(~type)
+
+cors %>% tidyr::spread(type, spearman) %>%
+  ggplot(aes(mean, slope)) + geom_point() +
+  geom_text(aes(label = method_id), hjust = 0.5, vjust = -0.5) +
+  xlab("Spearman correlation of B/BMSY mean") +
+  ylab("Spearman correlation of B/BMSY slope")
+
+re <- cv_sim_long %>% mutate(
+  sq_er = (bbmsy_est_trans - bbmsy_true_trans)^2,
+  re    = (bbmsy_est_trans - bbmsy_true_trans) / bbmsy_true_trans,
+  pe    = bbmsy_est_trans / bbmsy_true_trans)
+
+# make a data frame that summarizes all these performance metrics at once:
+re2 <- re %>% group_by(type, method_id) %>%
   summarize(
-    mare = median(abs(re), na.rm = TRUE),
-    rmse = sqrt(mean(sq_er, na.rm = TRUE)))
+    performance = median(abs(re), na.rm = TRUE),
+    performance_l = quantile(abs(re), 0.25, na.rm = TRUE),
+    performance_u = quantile(abs(re), 0.75, na.rm = TRUE)) %>%
+  mutate(summary = "MARE")
 
-ggplot(re, aes(method_id, mare)) + geom_boxplot()
-ggplot(re, aes(method_id, rmse)) + geom_boxplot()
+cors2 <- cors %>% mutate(
+  summary = "spearman",
+  performance = spearman,
+  performance_l = spearman,
+  performance_u = spearman) %>%
+  select(-spearman)
+
+performance <- bind_rows(re2, cors2) %>% as.data.frame
+
+performance %>% reshape2::dcast(method_id + type ~ summary, value.var = c("performance")) %>%
+  ggplot(aes(MARE, spearman)) + geom_point() + geom_text(aes(label = method_id), hjust = 0.2) +
+  facet_wrap(~type, scales = "free") +
+  xlab("MARE within stocks") + ylab("Spearman correlation across stocks")
