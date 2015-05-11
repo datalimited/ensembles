de62bda11c93956c4f7fab074c09c822cce17a13
Sean Anderson <sean@seananderson.ca>
2015-05-11 13:31:51 -0700

    Add commented-out code to debug CMSY unknown resilience

--- 2-run-catch-models.R
+++ 2-run-catch-models.R
@@ -4,78 +4,111 @@ load_all("../datalimited")
 
 ramts <- readRDS("generated-data/ramts.rds")
 
+# unknowns <- filter(ramts, res == "")
+# unknowns <- unique(unknowns$stockid)
+
 # Re-fit catch-MSY because there was an issue with the stochastic sigmaR:
 message("Running CMSY fits to RAM database...")
 ram_cmsy <- ramts %>%
   filter(stockid %in% unique(ramts$stockid)) %>%
+# filter(stockid %in% unknowns) %>%
   group_by(stockid) %>%
   do({
-    # message(.$stocklong[1])
     cmsy_out <- cmsy(
       yr             = .$year,
       ct             = .$catch,
       prior_log_mean = .$log_mean[1],
       prior_log_sd   = .$log_sd[1],
-      start_r        = resilience(.$res[1]),
+      start_r        = resilience(.$res[1], unknown_equals_medium = TRUE),
       sig_r          = 0.05,
-      reps           = 5000L)
+      reps           = 10000L)
     if(!is.null(cmsy_out)) {
       bbmsy <- cmsy_out$biomass[, -1] / cmsy_out$bmsy
       bbmsy_out <- summarize_bbmsy(bbmsy, na.rm = TRUE, log = TRUE)
       data.frame(year = .$year, c_touse = .$catch, bbmsy_out)
     } else {
-      data.frame(year = .$year, c_touse = .$catch, bbmsy_out = rep(NA, nrow(.)))
+      data.frame(year = .$year, c_touse = .$catch,
+        bbmsy_out = rep(NA, nrow(.)))
     }
   }) %>% as.data.frame
 ram_cmsy <- select(ram_cmsy, -bbmsy_out)
+# saveRDS(ram_cmsy, file = "~/Desktop/ram_cmsy.rds")
 
-#message("Running COM-SIR fits to RAM database...")
-#ram_comsir <- ramts %>%
-#  filter(stockid %in% unique(ramts$stockid)[c(1:6)]) %>%
-#  plyr::ddply("stockid", function(.) {})
-#group_by(stockid) %>%
-#  do({
-#    # message(.$stocklong[1])
+# ram_cmsy_unknown_equal_medium <- ramts %>%
+#   #filter(stockid %in% unique(ramts$stockid)) %>%
+#   filter(stockid %in% unknowns) %>%
+#   group_by(stockid) %>%
+#   do({
+#     cmsy_out <- cmsy(
+#       yr             = .$year,
+#       ct             = .$catch,
+#       prior_log_mean = .$log_mean[1],
+#       prior_log_sd   = .$log_sd[1],
+#       start_r        = resilience(.$res[1], unknown_equals_medium = TRUE),
+#       sig_r          = 0.05,
+#       reps           = 5000L)
+#     if(!is.null(cmsy_out)) {
+#       bbmsy <- cmsy_out$biomass[, -1] / cmsy_out$bmsy
+#       bbmsy_out <- summarize_bbmsy(bbmsy, na.rm = TRUE, log = TRUE)
+#       data.frame(year = .$year, c_touse = .$catch, bbmsy_out)
+#     } else {
+#       data.frame(year = .$year, c_touse = .$catch,
+#         bbmsy_out = rep(NA, nrow(.)))
+#     }
+#   }) %>% as.data.frame
+# ram_cmsy_unknown_equal_medium <- select(ram_cmsy_unknown_equal_medium,
+#   -bbmsy_out)
+# saveRDS(ram_cmsy_unknown_equal_medium,
+#   file = "~/Desktop/ram_cmsy_unknown_equal_medium.rds")
+#
+# #message("Running COM-SIR fits to RAM database...")
+# #ram_comsir <- ramts %>%
+# #  filter(stockid %in% unique(ramts$stockid)[c(1:6)]) %>%
+# #  plyr::ddply("stockid", function(.) {})
+# #group_by(stockid) %>%
+# #  do({
+# `.` <- subset(ramts, stockid == unique(ramts$stockid)[2])
+# #    # message(.$stocklong[1])
 #    comsir_out <- comsir(
 #      yr             = .$year,
-#      ct             = .$c_touse,
-#      x              = 0.5,
+#      ct             = .$catch,
+#      x              = 0.8,
 #      k              = 800,
 #      r              = 0.6,
 #      a              = 0.8,
 #      start_r        = resilience(.$res[1]),
-#      # start_r        = c(0.2, 1.0),
 #      norm_k         = FALSE,
 #      logk           = TRUE,
 #      norm_r         = FALSE,
 #      norm_a         = FALSE,
 #      norm_x         = FALSE,
 #      normal_like    = FALSE,
-#      nsim           = 1e6, # was 1e6
+#      nsim           = 2e6, # was 1e6
 #      cv             = 0.4,
 #      logistic_model = TRUE,
 #      obs            = FALSE,
 #      n_posterior    = 5e3) # was 5e3
-#    if(!is.null(comsir_out)) {
+# #    if(!is.null(comsir_out)) {
 #      bbmsy <- reshape2::dcast(comsir_out$quantities, sample_id ~ yr,
 #        value.var = "bbmsy")[,-1] # convert long to wide format
-#      # TODO switch log to TRUE:
+# #      # TODO switch log to TRUE:
 #      bbmsy_out <- summarize_bbmsy(bbmsy, log = FALSE, na.rm = TRUE)
-#      data.frame(year = .$year, c_touse = .$c_touse, bbmsy_out)
-#    } else {
-#      data.frame(year = .$year, c_touse = .$c_touse, bbmsy_out = rep(NA, nrow(.)))
-#    }
-#  }) %>% as.data.frame
+#      data.frame(year = .$year, catch = .$catch, bbmsy_out)
+# #    } else {
+# #      data.frame(year = .$year, catch = .$catch, bbmsy_out = rep(NA, nrow(.)))
+# #    }
+# #  }) %>% as.data.frame
+#
+# library("ggplot2")
+# # ggplot(ram_cmsy, aes(year, c_touse)) + geom_line() +
+# #   facet_wrap(~stockid, scales = "free_x")
+#
+# # ram_cmsy$bbmsy_q25[ram_cmsy$bbmsy_q25 < 0] <- 0
+# # ram_cmsy$bbmsy_q75[ram_cmsy$bbmsy_q75 > 2.5] <- 2.5
+# # ram_cmsy$bbmsy_q2.5[ram_cmsy$bbmsy_q2.5 < 0] <- 0
+# # ram_cmsy$bbmsy_q97.5[ram_cmsy$bbmsy_q97.5 > 2.5] <- 2.5
 
 library("ggplot2")
-# ggplot(ram_cmsy, aes(year, c_touse)) + geom_line() +
-#   facet_wrap(~stockid, scales = "free_x")
-
-# ram_cmsy$bbmsy_q25[ram_cmsy$bbmsy_q25 < 0] <- 0
-# ram_cmsy$bbmsy_q75[ram_cmsy$bbmsy_q75 > 2.5] <- 2.5
-# ram_cmsy$bbmsy_q2.5[ram_cmsy$bbmsy_q2.5 < 0] <- 0
-# ram_cmsy$bbmsy_q97.5[ram_cmsy$bbmsy_q97.5 > 2.5] <- 2.5
-
 p <- left_join(ram_cmsy, ramts[,c("year", "stockid", "stocklong", "bbmsy_ram"), ])
 saveRDS(p, file = "generated-data/ram-cmsy-2015.rds")
 
@@ -114,3 +147,15 @@ dev.off()
 # ggplot(ram_comsir, aes(year, bbmsy_q50)) + geom_point() +
 #   facet_wrap(~stockid)
 
+# names(ram_cmsy)[3:length(names(ram_cmsy))] <- paste0(names(ram_cmsy)[3:length(names(ram_cmsy))], "_notequal")
+#
+# x <- ram_cmsy %>%
+#   select(stockid, year, bbmsy_q50_notequal, bbmsy_q2.5_notequal, bbmsy_q97.5_notequal) %>%
+#   inner_join(select(ram_cmsy_unknown_equal_medium, stockid, year, bbmsy_q50, bbmsy_q2.5, bbmsy_q97.5))
+#
+#
+# xx <- reshape2::melt(x, id.vars = c("stockid", "year"))
+#
+# library(ggplot2)
+# p <- ggplot(xx, aes(year, value, colour = variable)) + facet_wrap(~stockid) + geom_line() + geom_hline(yintercept = 1, lty = 2) + theme_bw()
+# ggsave("~/Desktop/resilience-check.pdf", width = 13, height = 8)
