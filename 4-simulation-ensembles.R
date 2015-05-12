6feff4bca030c6436e78cdf3eefb4240186e4c79
Sean Anderson <sean@seananderson.ca>
2015-05-12 16:52:08 -0700

    Updates to plots and formatting, nothing major

--- 4-simulation-ensembles.R
+++ 4-simulation-ensembles.R
@@ -12,8 +12,8 @@ dsim <- rename(dsim, stockid = stock_id, method = method_id) # to match RAM fits
 # (note that there are still multiple iterations with the same stockid)
 # TODO: should 'scenario' be across sigmaC and sigmaR values too?
 dsim <- dsim %>%
-  transform(stockid =
-  paste0(stockid, "_sigmaC_", sigmaC, "_sigmaR_", sigmaR, "_lh", LH)) %>%
+  mutate(stockid =
+    paste0(stockid, "_sigmaC_", sigmaC, "_sigmaR_", sigmaR, "_lh", LH)) %>%
   arrange(stockid, iter, year) # critical since not all in order
 
 # summarise the mean and slope in the last N years:
@@ -68,7 +68,6 @@ partial <- plyr::ldply(seq_len(5), function(i) {
 })
 
 # partial dependence plot:
-ggplot(partial, aes(predictor_value, y)) + geom_line() + facet_wrap(~predictor) + xlim(0, 3)
 p <- ggplot(partial, aes(predictor_value, y)) + geom_line() +
   facet_wrap(~predictor) + xlim(0, 3)
 ggsave("figs/partial-sim.pdf", width = 7, height = 5)
@@ -102,18 +101,17 @@ dev.off()
 message(paste("zlim were", round(zlim, 2), collapse = " "))
 
 # work through cross validation of ensemble models:
-library("doParallel")
-registerDoParallel(cores = 4)
-
-cv_sim_mean <- plyr::ldply(seq_len(8), .parallel = TRUE,
+cv_sim_mean <- plyr::ldply(seq_len(4), .parallel = TRUE,
   .fun = function(.n)
     cross_val_ensembles(.n = .n, dat = d_mean, geo_mean = TRUE, id = "sim-mean",
       gbm_formula = "log(bbmsy_true_mean) ~ CMSY + COMSIR + Costello + SSCOM + LH",
       lm_formula = "log(bbmsy_true_mean) ~ (CMSY + COMSIR + Costello + SSCOM + LH)^2"))
 cv_sim_mean$gbm_ensemble <- exp(cv_sim_mean$gbm_ensemble)
+cv_sim_mean$gbm2_ensemble <- exp(cv_sim_mean$gbm2_ensemble)
+#cv_sim_mean$rf_ensemble <- exp(cv_sim_mean$rf_ensemble)
 cv_sim_mean$lm_ensemble <- exp(cv_sim_mean$lm_ensemble)
 
-cv_sim_slope <- plyr::ldply(seq_len(8), .parallel = TRUE,
+cv_sim_slope <- plyr::ldply(seq_len(4), .parallel = TRUE,
   .fun = function(.n)
     cross_val_ensembles(.n = .n, dat = d_slope, geo_mean = FALSE, id = "sim-slope",
      gbm_formula = "bbmsy_true_slope ~ CMSY + COMSIR + Costello + SSCOM + LH",
@@ -147,26 +145,26 @@ cv_sim_slope_long <- reshape2::melt(select(cv_sim_slope, -above_bbmsy1_true),
     bbmsy_true_trans = bbmsy_true,
     bbmsy_est_trans = bbmsy_est)
 
-ggplot(cv_sim_mean_long, aes(bbmsy_true, bbmsy_est)) +
-  geom_point(alpha = 0.01) +
-    facet_wrap(~method) + ylim(0, 3) + xlim(0, 3)
-
-ggplot(cv_sim_slope_long, aes(bbmsy_true, bbmsy_est)) +
-  geom_point(alpha = 0.01) +
-    facet_wrap(~method) + xlim(-.5, .5) + ylim(-.5, .5)
+# ggplot(cv_sim_mean_long, aes(bbmsy_true, bbmsy_est)) +
+#   geom_point(alpha = 0.01) +
+#     facet_wrap(~method) + ylim(0, 3) + xlim(0, 3)
+#
+# ggplot(cv_sim_slope_long, aes(bbmsy_true, bbmsy_est)) +
+#   geom_point(alpha = 0.01) +
+#     facet_wrap(~method) + xlim(-.5, .5) + ylim(-.5, .5)
 
 cv_sim_long <- suppressWarnings(
   dplyr::bind_rows(cv_sim_mean_long, cv_sim_slope_long))
 saveRDS(cv_sim_long, "generated-data/cv_sim_long.rds")
 
-cors <- cv_sim_long %>% group_by(method, type) %>%
-  summarise(spearman = cor(bbmsy_true_trans, bbmsy_est_trans, method = "spearman",
-    use = "pairwise.complete.obs")) %>% as.data.frame %>%
-  mutate(spearman = round(spearman, 4)) %>%
-  arrange(type, -spearman)
+# cors <- cv_sim_long %>% group_by(method, type) %>%
+#   summarise(spearman = cor(bbmsy_true_trans, bbmsy_est_trans, method = "spearman",
+#     use = "pairwise.complete.obs")) %>% as.data.frame %>%
+#   mutate(spearman = round(spearman, 4)) %>%
+#   arrange(type, -spearman)
 
-ggplot(cors, aes(x = method, xend = method, yend = spearman)) +
-  geom_segment(y = 0, lwd = 1.2) + facet_wrap(~type)
+# ggplot(cors, aes(x = method, xend = method, yend = spearman)) +
+#   geom_segment(y = 0, lwd = 1.2) + facet_wrap(~type)
 
 cors %>% tidyr::spread(type, spearman) %>%
   ggplot(aes(mean, slope)) + geom_point() +
@@ -196,7 +194,11 @@ cors2 <- cors %>% mutate(
 
 performance <- bind_rows(re2, cors2) %>% as.data.frame
 
-performance %>% reshape2::dcast(method + type ~ summary, value.var = c("performance")) %>%
-  ggplot(aes(MARE, spearman)) + geom_point() + geom_text(aes(label = method), hjust = 0.2) +
+p <- performance %>%
+  reshape2::dcast(method + type ~ summary, value.var = c("performance")) %>%
+  ggplot(aes(MARE, spearman)) +
+  #geom_point() +
+  geom_text(aes(label = method), hjust = 0.2, size = 3) +
   facet_wrap(~type, scales = "free") +
   xlab("MARE within stocks") + ylab("Spearman correlation across stocks")
+ggsave("figs/performance-sim-scatter.pdf", width = 8, height = 4)
