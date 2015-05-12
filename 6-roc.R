db748a21e02c75ad2935f8c4f34c9b8578562412
Sean Anderson <sean@seananderson.ca>
2015-05-12 15:18:27 -0700

    Rename ROC code file

--- 6-roc.R
+++ 6-roc.R
@@ -12,9 +12,9 @@ get_roc <- function(true, est) {
 #   above1_est = ifelse(bbmsy_est > 1, 1, 0),
 #   above1_true = ifelse(bbmsy_true > 1, 1, 0))
 #
-# rocs <- filter(cv_sim_long, type == "mean") %>% group_by(method_id) %>%
+# rocs <- filter(cv_sim_long, type == "mean") %>% group_by(method) %>%
 #   do({get_roc(true = .$above1_true, est = .$above1_est)})
-# ggplot(rocs, aes(spec, sens, colour = method_id)) + geom_line()
+# ggplot(rocs, aes(spec, sens, colour = method)) + geom_line()
 
 cv_sim_binary <- readRDS("generated-data/cv_sim_binary.rds") %>% as.data.frame
 
@@ -27,13 +27,13 @@ cv_sim_binary <- readRDS("generated-data/cv_sim_binary.rds") %>% as.data.frame
 
 # switch to long format for ROCing:
 cv_sim_binary_long <- select(cv_sim_binary, -bbmsy_true_mean) %>%
- reshape2::melt(id.vars = c("stock_id", "iter", "test_iter", "LH", "above_bbmsy1_true"),
-  variable.name = "method_id", value.name = "bbmsy_est")
+ reshape2::melt(id.vars = c("stockid", "iter", "test_iter", "LH", "above_bbmsy1_true"),
+  variable.name = "method", value.name = "bbmsy_est")
 
-rocs <- cv_sim_binary_long %>% group_by(method_id) %>%
+rocs <- cv_sim_binary_long %>% group_by(method) %>%
   do({get_roc(true = .$above_bbmsy1_true, est = .$bbmsy_est)})
 
-p <- ggplot(rocs, aes(spec, sens, colour = method_id)) + geom_line() + coord_equal() +
+p <- ggplot(rocs, aes(spec, sens, colour = method)) + geom_line() + coord_equal() +
   geom_abline(intercept = 1, slope = 1, lty = 2, col = "darkgrey") +
   xlim(1, 0) +
   xlab("Specificity") + ylab("Sensitivity") + theme_bw() +
