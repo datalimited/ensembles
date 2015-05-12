a01f8f45b45d4a9a256ba154fc54c4106f0102de
Sean Anderson <sean@seananderson.ca>
2015-05-12 16:52:31 -0700

    Clean up ROC script; save plots

--- 6-roc.R
+++ 6-roc.R
@@ -7,16 +7,8 @@ get_roc <- function(true, est) {
     auc = as.numeric(y$auc))
 }
 
-# cv_sim_long <- readRDS("generated-data/cv_sim_long.rds") %>% as.data.frame
-# cv_sim_long <- cv_sim_long %>% mutate(
-#   above1_est = ifelse(bbmsy_est > 1, 1, 0),
-#   above1_true = ifelse(bbmsy_true > 1, 1, 0))
-#
-# rocs <- filter(cv_sim_long, type == "mean") %>% group_by(method) %>%
-#   do({get_roc(true = .$above1_true, est = .$above1_est)})
-# ggplot(rocs, aes(spec, sens, colour = method)) + geom_line()
-
 cv_sim_binary <- readRDS("generated-data/cv_sim_binary.rds") %>% as.data.frame
+cv_ram_binary <- readRDS("generated-data/cv_ram_binary.rds") %>% as.data.frame
 
 # cv_sim_binary <- cv_sim_binary %>% mutate(
 #   CMSY = ifelse(CMSY > 1, 1, 0),
@@ -30,14 +22,30 @@ cv_sim_binary_long <- select(cv_sim_binary, -bbmsy_true_mean) %>%
  reshape2::melt(id.vars = c("stockid", "iter", "test_iter", "LH", "above_bbmsy1_true"),
   variable.name = "method", value.name = "bbmsy_est")
 
-rocs <- cv_sim_binary_long %>% group_by(method) %>%
+cv_ram_binary_long <- select(cv_sim_binary, -bbmsy_true_mean) %>%
+ reshape2::melt(id.vars = c("stockid", "test_iter", "habitat", "above_bbmsy1_true"),
+  variable.name = "method", value.name = "bbmsy_est")
+
+rocs_sim <- cv_sim_binary_long %>% group_by(method) %>%
   do({get_roc(true = .$above_bbmsy1_true, est = .$bbmsy_est)})
 
-p <- ggplot(rocs, aes(spec, sens, colour = method)) + geom_line() + coord_equal() +
+rocs_ram <- cv_ram_binary_long %>% group_by(method) %>%
+  do({get_roc(true = .$above_bbmsy1_true, est = .$bbmsy_est)})
+
+p <- ggplot(rocs_sim, aes(spec, sens, colour = method)) + geom_line() + coord_equal() +
   geom_abline(intercept = 1, slope = 1, lty = 2, col = "darkgrey") +
   xlim(1, 0) +
   xlab("Specificity") + ylab("Sensitivity") + theme_bw() +
   theme(plot.background = element_blank(),
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank())
-print(p)
+ggsave("figs/roc-sim.pdf", width = 7, height = 5)
+
+p <- ggplot(rocs_ram, aes(spec, sens, colour = method)) + geom_line() + coord_equal() +
+  geom_abline(intercept = 1, slope = 1, lty = 2, col = "darkgrey") +
+  xlim(1, 0) +
+  xlab("Specificity") + ylab("Sensitivity") + theme_bw() +
+  theme(plot.background = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank())
+ggsave("figs/roc-ram.pdf", width = 7, height = 5)
