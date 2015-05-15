a69ed64a2f5de534cd60e47049935b244938e858
Sean Anderson <sean@seananderson.ca>
2015-05-14 17:32:55 -0700

    Switch cross-validation to true k-fold cross-validation

--- 4-ensemble-functions.R
+++ 4-ensemble-functions.R
@@ -19,53 +19,55 @@ cross_val_ensembles <- function(.n, dat, fraction_train = 0.5,
   geo_mean = TRUE,
   individual_models = c("CMSY", "COMSIR", "Costello", "SSCOM"),
   id = "mean", cache_folder = "generated-data/cv-sim/", distribution = "gaussian",
-  lm_formula = "", glm_formula = "") {
+  lm_formula = "", glm_formula = "", nfold = 3L) {
 
-  cv_ids_set <- FALSE # legacy code, leaving in in case needed for RAM stocks
-  while(!cv_ids_set) {
-    n_ids <- length(unique(dat$cv_id))
-    train_ids <- sample(n_ids, round(n_ids * fraction_train))
-    test_ids <- seq_len(n_ids)[-train_ids]
+  ids <- unique(dat$cv_id)
+  ids_scrambled <- base::sample(ids)
+  chunk_size <- floor(length(ids) / nfold)
+  chunk_starts <- seq(1L, chunk_size * nfold, chunk_size)
+  # excluding those that aren't used in this sorting due to rounding:
+  ids_scrambled_cut <- ids_scrambled[seq_len(chunk_size * nfold)]
 
-    train_cv_ids <- unique(dat$cv_id)[train_ids]
-    test_cv_ids <- unique(dat$cv_id)[test_ids]
+  out <- plyr::ldply(seq_along(chunk_starts), function(i) {
 
-    train_dat <- filter(dat, cv_id %in% train_cv_ids)
-    test_dat <- filter(dat, cv_id %in% test_cv_ids)
+    test_ids  <- ids_scrambled_cut[seq(chunk_starts[i], chunk_starts[i] + chunk_size)]
+    train_ids <- ids_scrambled_cut[!ids_scrambled_cut %in% test_ids]
+    train_dat <- dplyr::filter(dat, cv_id %in% train_ids)
+    test_dat  <- dplyr::filter(dat, cv_id %in% test_ids)
 
-    cv_ids_set <- TRUE
-  }
+    m_gbm <- gbm::gbm(as.formula(gbm_formula),
+      data = train_dat, n.trees = 10000L, interaction.depth = 2, shrinkage = 0.01,
+      distribution = distribution)
+    #saveRDS(m_gbm, file = paste0(cache_folder, id, "-", .n, "-gbm.rds"))
 
-  m_gbm <- gbm::gbm(as.formula(gbm_formula),
-    data = train_dat, n.trees = 10000L, interaction.depth = 2, shrinkage = 0.01,
-    distribution = distribution)
-  #saveRDS(m_gbm, file = paste0(cache_folder, id, "-", .n, "-gbm.rds"))
-  test_dat$gbm_ensemble <- tryCatch({gbm::predict.gbm(m_gbm,
-    n.trees = m_gbm$n.trees, newdata = test_dat, type = "response")},
-    error = function(e) rep(NA, nrow(test_dat)))
-
-  m_rf <- randomForest::randomForest(as.formula(gbm_formula), data = train_dat)
-  test_dat$rf_ensemble <- tryCatch({predict(m_rf, newdata = test_dat)},
-    error = function(e) rep(NA, nrow(test_dat)))
-
-  if (lm_formula != "") {
-    m_lm <- lm(as.formula(lm_formula), data = train_dat)
-    test_dat$lm_ensemble <- tryCatch({predict(m_lm, newdata = test_dat)},
+    test_dat$gbm_ensemble <- tryCatch({gbm::predict.gbm(m_gbm,
+      n.trees = m_gbm$n.trees, newdata = test_dat, type = "response")},
       error = function(e) rep(NA, nrow(test_dat)))
-  }
 
-  if (glm_formula != "") {
-    m_glm <- glm(as.formula(glm_formula), data = train_dat, family = binomial(link = logit))
-    test_dat$glm_ensemble <- tryCatch({predict(m_glm, newdata = test_dat, type = "response")},
+    m_rf <- randomForest::randomForest(as.formula(gbm_formula), data = train_dat)
+    test_dat$rf_ensemble <- tryCatch({predict(m_rf, newdata = test_dat)},
       error = function(e) rep(NA, nrow(test_dat)))
-  }
 
-  if (geo_mean) {
-    test_dat$mean_ensemble <- exp(rowMeans(log(test_dat[, individual_models])))
-  } else {
-    test_dat$mean_ensemble <- rowMeans(test_dat[, individual_models])
-  }
+    if (lm_formula != "") {
+      m_lm <- lm(as.formula(lm_formula), data = train_dat)
+      test_dat$lm_ensemble <- tryCatch({predict(m_lm, newdata = test_dat)},
+        error = function(e) rep(NA, nrow(test_dat)))
+    }
 
-  test_dat$test_iter <- .n
-  test_dat
+    if (glm_formula != "") {
+      m_glm <- glm(as.formula(glm_formula), data = train_dat, family = binomial(link = logit))
+      test_dat$glm_ensemble <- tryCatch({predict(m_glm, newdata = test_dat, type = "response")},
+        error = function(e) rep(NA, nrow(test_dat)))
+    }
+
+    if (geo_mean) {
+      test_dat$mean_ensemble <- exp(rowMeans(log(test_dat[, individual_models])))
+    } else {
+      test_dat$mean_ensemble <- rowMeans(test_dat[, individual_models])
+    }
+
+    test_dat$test_iter <- .n
+    test_dat
+  })
+  out
 }
