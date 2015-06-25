a547c90551fbf4d30428ce9cc05e5bf91a7769e8
Sean Anderson <sean@seananderson.ca>
2015-06-25 16:07:33 -0700

    Substitute locally fit version of mPRM
    - Use recreated species categories
    - Note the coefficients are not identical, but impossible to debug

--- analysis/2-merge-ram-fits.R
+++ analysis/2-merge-ram-fits.R
@@ -57,4 +57,55 @@ ram_fits <- rename(ram_fits,
   b_bmsy_true = b2bmsy_true,
   b_bmsy_est = b2bmsy)
 
+# refit the mPRM fits with the 3-level species categories:
+sp_cat <- read.csv("raw-data/spp-3-lifehistory-categories.csv",
+  stringsAsFactors = FALSE, strip.white = TRUE, comment.char = "#") %>%
+  select(spname, LH2) %>%
+  unique() %>%
+  rename(scientificname = spname, spp_category = LH2) %>%
+  filter(!is.na(spp_category))
+stopifnot(sum(duplicated(sp_cat$spname)) == 0)
+
+# bring in the missing species I categorized:
+sp_cat2 <- read.csv("raw-data/missing-spp3.csv", strip.white = TRUE,
+  stringsAsFactors = FALSE, comment.char = "#") %>%
+  select(scientificname, species_cat) %>% unique() %>%
+  rename(spp_category = species_cat)
+stopifnot(sum(duplicated(sp_cat2$scientificname)) == 0)
+
+sp_cat <- bind_rows(sp_cat, sp_cat2)
+stopifnot(ncol(sp_cat) == 2)
+stopifnot(sum(is.na(sp_cat$spp_category)) == 0)
+
+ram2 <- select(ram, assessid, stockid, tsyear, stocklong, scientificname,
+  CtoUse, res, Bbmsy_toUse) %>%
+  filter(!is.na(CtoUse)) %>%
+  left_join(sp_cat, by = "scientificname")
+stopifnot(sum(is.na(ram2$spp_category)) == 0)
+
+ram_prm_dat <- plyr::ddply(ram2, "stockid", function(x) {
+  datalimited::format_prm(year = x$tsyear, catch = x$CtoUse, bbmsy = x$Bbmsy_toUse,
+    species_cat = x$spp_category[1L])
+})
+
+m <- datalimited::fit_prm(ram_prm_dat)
+
+mprm_ram <- plyr::ddply(ram_prm_dat, c("stockid"), function(x) {
+  out <- predict_prm(x, model = m, ci = TRUE, level = 0.95)
+  out$tsyear <- x$year
+  out
+})
+
+mprm_ram <- rename(mprm_ram, b2bmsy = fit) %>% mutate(method = "Costello") %>%
+  select(-lower, -upper)
+
+# do both have the same stocks?
+stopifnot(identical(setdiff(
+    unique(ram_fits$stockid), unique(mprm_ram$stockid)),
+    vector(mode = "character", length = 0L)))
+
+ram_fits <- filter(ram_fits, method != "Costello") %>%
+  filter(!is.na(stockid)) %>%
+  bind_rows(mprm_ram)
+
 saveRDS(ram_fits, "generated-data/ram_fits.rds")
