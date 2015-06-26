c73fd5a3d5023f00bc612338d96dca4000fadf5f
Sean Anderson <sean@seananderson.ca>
2015-06-25 17:45:17 -0700

    Tweak comments

--- analysis/3-load-sim-data.R
+++ analysis/3-load-sim-data.R
@@ -1,11 +1,14 @@
 library("dplyr")
 
 # Bring in all results exept CMSY:
-# TODO: absolute file path:
+# TODO: local file path:
 load("../../../Dropbox/FisheriesWorkingGroupPhaseII/Decision_trees/CMSY_COMSIR_SSCOM_COSTELLO_STO_BATCH_ALL_RESULTS 2013-06-24 .Rdata")
 
+# bringing in corrected fits below
 d1 <- dplyr::filter(batch1results, method_id != "CMSY")
+
 rm(batch1results)
+
 d1$stock_id <- as.character(d1$stock_id)
 d1$ID <- as.numeric(as.character(d1$ID))
 d1$b_bmsyUpper <- as.numeric(as.character(d1$b_bmsyUpper))
@@ -16,6 +19,7 @@ d1$seed <- as.numeric(as.character(d1$seed))
 d1$n_iterations <- as.numeric(as.character(d1$n_iterations))
 d1$effective_sample_size <- as.numeric(as.character(d1$effective_sample_size))
 
+# TODO: data only local:
 load("raw-data/cmsy_fits_22052015.RData")
 d2 <- cmsynewfits
 rm(cmsynewfits)
